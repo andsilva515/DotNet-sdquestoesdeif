1c9e8e73e88141dfb54fc691dcde780cc680be6a
andsilva515 <anderson.ssilva@outlook.com>
2025-06-16 16:50:51 -0300

    Interfaces genericas e especificas

---
+++ src/services/SoQuestoesIF.Domain/Interfaces/IAnswerHistoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoQuestoesIF.Domain.Interfaces
+{
+    public interface IAnswerHistoryRepository
+    {
+        // Métodos customizados para AnswerHistory
+    }
+}
