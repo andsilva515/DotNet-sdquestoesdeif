4569e0ef35efd5dd8a767c9581cd99982ac07a1a
andsilva515 <anderson.ssilva@outlook.com>
2025-06-16 09:10:53 -0300

    Entidades relacionadas simulado resposta

--- src/services/SoQuestoesIF.Domain/Entities/ExamQuestion.cs
+++ src/services/SoQuestoesIF.Domain/Entities/ExamQuestion.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace SoQuestoesIF.Domain.Entities
 {
-    internal class ExamQuestion
+    public class ExamQuestion
     {
+        public Guid ExamId { get; set; }
+        public Exam Exam { get; set; }
+
+        public Guid QuestionId { get; set; }
+
+        public Question Question { get; set; }
+
+        public int Order { get; set; }
     }
 }
