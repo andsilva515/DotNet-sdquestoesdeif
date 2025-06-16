ffa05b263b9a66ba2e179b65ba2bbac831cf2096
andsilva515 <anderson.ssilva@outlook.com>
2025-06-16 09:02:40 -0300

    Entidades relacionadas resposta

--- src/services/SoQuestoesIF.Domain/Entities/Answer.cs
+++ src/services/SoQuestoesIF.Domain/Entities/Answer.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace SoQuestoesIF.Domain.Entities
 {
-    internal class Answer
+    public class Answer
     {
+        public Guid Id { get; set; }
+        public DateTime AnsweredAt { get; set; }
+        public bool IsCorrect { get; set; }
+
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+
+        public Guid QuestionId { get; set; }
+        public Question Question { get; set; }
+
+        public Guid SelectedAlternativeId { get; set; }
+        public Alternative SelectedAlternative { get; set; }
+
+
     }
 }
