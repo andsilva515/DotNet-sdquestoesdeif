3fd2eef0b70f56c524263a93e3c06ad9da4fdc22
andsilva515 <anderson.ssilva@outlook.com>
2025-06-16 09:14:07 -0300

    Entidades relacionadas caderno personalizado

--- src/services/SoQuestoesIF.Domain/Entities/QuestionSet.cs
+++ src/services/SoQuestoesIF.Domain/Entities/QuestionSet.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace SoQuestoesIF.Domain.Entities
 {
-    internal class QuestionSet
+    public class QuestionSet
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+        public ICollection<QuestionSetQuestion> Questions { get; set; }
     }
 }
