ba7627d6578f5f863b35af26e580f5a9712353de
andsilva515 <anderson.ssilva@outlook.com>
2025-06-16 23:32:39 -0300

    Interfaces genericas e especificas

---
+++ src/services/SoQuestoesIF.Domain/Interfaces/IInstitutionRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoQuestoesIF.Domain.Interfaces
+{
+    public interface IInstitutionRepository
+    {
+        // Métodos customizados para Institution
+    }
+}
