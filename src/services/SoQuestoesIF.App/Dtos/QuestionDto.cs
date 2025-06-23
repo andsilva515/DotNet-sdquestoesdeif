3bc6bf51ad9d719cad95a4b16f5a729ab580eeb9
andsilva515 <anderson.ssilva@outlook.com>
2025-06-23 16:20:18 -0300

    Data Transfer Objects

---
+++ src/services/SoQuestoesIF.App/Dtos/QuestionDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoQuestoesIF.App.Dtos
+{
+    public class QuestionDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Statement { get; set; }
+        public string Type { get; set; }
+        public Guid SubjectId { get; set; }
+        public Guid DisciplineId { get; set; }
+        public Guid InstitutionId { get; set; }
+        public Guid ExamBoardId { get; set; }
+        public string Year { get; set; }
+        public string Answer { get; set; }
+        public string Explanation { get; set; }
+        public string Status { get; set; }
+        public string Difficulty { get; set; }
+        public string EditalUrl { get; set; }
+        public string ExamUrl { get; set; }
+
+    }
+}
---
+++ src/services/SoQuestoesIF.App/Dtos/UserNotebookDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoQuestoesIF.App.Dtos
+{
+    public class UserNotebookDto
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Title { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
