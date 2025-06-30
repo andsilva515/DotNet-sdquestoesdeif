2e8b6b797537d50838ea520c72a5839a71a1f7b4
andsilva515 <anderson.ssilva@outlook.com>
2025-06-30 17:15:30 -0300

    injecao de dependencia repository

--- src/services/SoQuestoesIF.API/Program.cs
+++ src/services/SoQuestoesIF.API/Program.cs
@@ -1,4 +1,7 @@
+using FluentAssertions.Common;
 using SoQuestoesIF.App.Mappings;
+using SoQuestoesIF.Domain.Interfaces;
+using SoQuestoesIF.Infra.Data.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +11,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+services.AddScoped<IUserRepository, UserRepository>();
+
+
 object value = builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
 
 var app = builder.Build();
