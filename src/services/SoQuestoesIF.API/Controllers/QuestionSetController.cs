7d737b4f60b9bec44c78d6d005b0b0b1baeee1bd
andsilva515 <anderson.ssilva@outlook.com>
2025-07-01 01:10:06 -0300

    Renomea Notebook para QuestionSet

--- src/services/SoQuestoesIF.API/Controllers/QuestionSetController.cs
+++ src/services/SoQuestoesIF.API/Controllers/QuestionSetController.cs
@@ -1,53 +1,55 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SoQuestoesIF.App.Services;
+using SoQuestoesIF.Domain.Entities;
 using SoQuestoesIF.Domain.Services;
 
 namespace SoQuestoesIF.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class UserNotebooksController : ControllerBase
+    public class QuestionSetController : ControllerBase
     {
-        private readonly IQuestionSetService _usernotebookService;
-        public UserNotebooksController(IQuestionSetService usernotebookService)
+        private readonly IQuestionSetService _questionsetService;
+        public QuestionSetController(IQuestionSetService questionsetService)
         {
-            _usernotebookService = usernotebookService;
+            _questionsetService = questionsetService;
         }
 
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
-            var items = await _usernotebookService.GetAllAsync();
+            var items = await _questionsetService.GetAllAsync();
             return Ok(items);
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById(Guid id)
         {
-            var item = await _usernotebookService.GetByIdAsync(id);
+            var item = await _questionsetService.GetByIdAsync(id);
             if (item == null) return NotFound();
             return Ok(item);
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create(UserNotebook usernotebook)
+        public async Task<IActionResult> Create(QuestionSet usernotebook)
         {
-            await _usernotebookService.AddAsync(usernotebook);
+            await _questionsetService.AddAsync(usernotebook);
             return CreatedAtAction(nameof(GetById), new { id = usernotebook.Id }, usernotebook);
         }
 
         [HttpPut("{id}")]
-        public async Task<IActionResult> Update(Guid id, UserNotebook usernotebook)
+        public async Task<IActionResult> Update(Guid id, QuestionSet usernotebook)
         {
             if (id != usernotebook.Id) return BadRequest();
-            await _usernotebookService.UpdateAsync(usernotebook);
+            await _questionsetService.UpdateAsync(usernotebook);
             return NoContent();
         }
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(Guid id)
         {
-            await _usernotebookService.DeleteAsync(id);
+            await _questionsetService.DeleteAsync(id);
             return NoContent();
         }
     }
