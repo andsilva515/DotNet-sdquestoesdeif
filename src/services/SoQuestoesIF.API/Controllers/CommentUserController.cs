40629ed166657d997dc34b7f35653042ec49ba14
andsilva515 <anderson.ssilva@outlook.com>
2025-07-01 01:00:39 -0300

    Renomea Comment para CommentUser

--- src/services/SoQuestoesIF.API/Controllers/CommentUserController.cs
+++ src/services/SoQuestoesIF.API/Controllers/CommentUserController.cs
@@ -7,48 +7,48 @@ namespace SoQuestoesIF.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CommentsController : ControllerBase
+    public class CommentUserController : ControllerBase
     {
-        private readonly ICommentService _commentService;
+        private readonly ICommentUserService _commentuserService;
 
-        public CommentsController(ICommentService commentService)
+        public CommentUserController(ICommentUserService commentUserService)
         {
-            _commentService = commentService;
+            _commentuserService = commentUserService;
         }
 
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
-            var items await _commentService.GetAllAsync();
+            var items await _commentuserService.GetAllAsync();
             return Ok(items);
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById(Guid id)
         {
-            var item = await _commentService.GetByIdAsync(id);
+            var item = await _commentuserService.GetByIdAsync(id);
             if (item == null) return NotFound();
             return Ok(item);
         }
         [HttpPost]
-        public async Task<IActionResult> Create(Comment comment)
+        public async Task<IActionResult> Create(CommentUser comment)
         {
-            await _commentService.AddAsync(comment);
+            await _commentuserService.AddAsync(comment);
             return CreatedAtAction(nameof(GetById), new { id = comment.Id }, comment);
         }
 
         [HttpPut("{id}")]
-        public async Task<IActionResult> Update(Guid id, Comment comment)
+        public async Task<IActionResult> Update(Guid id, CommentUser comment)
         {
             if (id != comment.Id) return BadRequest();
-            await _commentService.UpdateAsync(comment);
+            await _commentuserService.UpdateAsync(comment);
             return NoContent();
         }
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(Guid id)
         {
-            await _commentService.DeleteAsync(id);
+            await _commentuserService.DeleteAsync(id);
             return NoContent();
         }
     }
